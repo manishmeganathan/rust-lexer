362bba6369b53fa512293e400c58aba74b76df9e
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-18 20:16:45 +0530

    Update main.rs
    - Added a welcome statement when the program is run

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod readers;
 use std::io::{self, Write};
 
 fn main() {
+    print!("Welcome to the lexer. Input any series of characters.\n");
     loop {
         print!("lexer >: ");
         io::stdout().flush().unwrap();
