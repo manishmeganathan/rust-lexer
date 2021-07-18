6499d84993198f7ce64d767f92298b7b9cbebd83
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-18 19:07:45 +0530

    Create lex utilities
    - Added a module utils.rs
    - Added functions to check if a character is a digit or a letter
    - Added a function to lookup a keyword token for an identifier

---
+++ src/utils.rs
@@ -0,0 +1,25 @@
+// This module contains utility functions for the lexer
+
+use crate::token::TokenType;
+
+// A function that checks if a character is a letter.
+pub fn is_letter(ch: char) -> bool {
+    'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// A function that checks if a character is a digit.
+pub fn is_digit(ch: char) -> bool {
+    '0' <= ch && ch <= '9'
+}
+
+// A function that returns a Token for a keyword identifier
+pub fn lookup_keyword(data: &Vec<char>) -> Result<TokenType, String> {
+    let content: String = data.into_iter().collect();
+
+    match &content[..] {
+        "TRUE" => Ok(TokenType::TRUE),
+        "FALSE" => Ok(TokenType::FALSE),
+        "ALL" => Ok(TokenType::ALL),
+        _ => Err(String::from("Not a keyword"))
+    }
+}
