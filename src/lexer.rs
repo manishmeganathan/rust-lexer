db3becfc183252d5f25fda2f8dc2dc910dac2854
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-18 19:35:33 +0530

    Add Lex navigation
    - Added a method of Lexer that eats whitespaces between characters while lexing the source code
    - Added a method of Lexer that advances the Lexer and lexes the ingested character into a Token.
    (simple logic tree for now)

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,8 @@
 // This module contains the Lexer struct and its implementations
 
+use crate::token::{Token, TokenType};
+use crate::utils::{is_alpha, is_digit, lookup_keyword};
+
 // A structure that represents a Lexer
 pub struct Lexer {
     pub source: Vec<char>,
@@ -31,3 +34,79 @@ impl Lexer {
     } 
 }
 
+// Navigation methods of the Lexer
+impl Lexer {
+    // A method of Lexer that reads and consumes all white 
+    // spaces until the next non whitespace character
+    fn eat_whitespaces(&mut self) {
+        // Iterate until character is a non white space character
+        while self.ch == ' ' || self.ch == '\t' || self.ch == '\n' || self.ch == '\r' {
+            // Read the character and consume it
+            self.read_char();
+        }
+    }
+
+    // A method of Lexer that lexes the current char 
+    // into a Token and advances the lex cursor
+    fn next_token(&mut self) -> Token {
+        // Declare a Token
+        let token: Token;
+        // Consume any white space chars
+        self.eat_whitespaces();
+
+        // Check the value of the current char
+        match self.ch {
+            // EOF character
+            '0' => {
+                // Generate an EOF Token
+                token = Token::new(TokenType::EOF, "".to_string());
+            },
+
+            // Not a single character 
+            _ => {
+                // Check if char is a letter
+                if is_alpha(self.ch) {
+                    // Read full identifier
+                    let ident = self.read_identifier();
+                    // Convert vector char into a string
+                    let identstr = ident.clone().into_iter().collect();
+
+                    // Check if the identifer is a keyword
+                    match lookup_keyword(&ident) {
+                        // Keyword
+                        Ok(keyword_token) => {
+                            // Generate the appropriate Keyword Token
+                            token = Token::new(keyword_token, identstr);
+                        },
+
+                        // Identifier 
+                        Err(_err) => {
+                            // Generate an Identifier Token
+                            token = Token::new(TokenType::IDENT, identstr);
+                        }
+                    }
+
+                // Check if char is a digit
+                } else if is_digit(self.ch) {
+                    // Read full number
+                    let number = self.read_number();
+                    // Convert vector char into a string
+                    let numberstr = number.clone().into_iter().collect();
+
+                    // Generate an INT Token
+                    token = Token::new(TokenType::INT, numberstr);
+                
+                // Illegal char
+                } else {
+                    // Generate an Illegal Token
+                    token = Token::new(TokenType::ILLEGAL, self.ch.to_string());
+                }
+            }
+        }
+
+        // Advance the lex parser
+        self.read_char();
+        // Return the parsed token
+        return token;
+    }
+}
